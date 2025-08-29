0860edb21b650abc63c0f0f9217740169f403f7d
Valentin Barrenechea <barrenecheavale@gmail.com>
2025-08-28 21:57:21 -0300

    Commit prueba

--- views/inicio.js
+++ views/inicio.js
@@ -2,6 +2,6 @@ const layout = require('./layout');
 
 module.exports = ({ req }) => {
     return layout({content:`
-        <div> Pediloooooooooooooo </div>
+        <div> Pediloooooooooooooo </div>wdadwawdwa
     `});
 };
